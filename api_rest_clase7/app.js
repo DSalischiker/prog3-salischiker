5755baa487a14f2f470a8e59615fd5ff47b499e7
dieguito711 <diegosalischiker711@gmail.com>
2020-05-02 16:38:21 -0300

    Revert "Revert "Clase 7 - API Rest""
    This reverts commit 834aae639201aa93e8628a7301ba5742e0efd396.

---
+++ api_rest_clase7/app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var http = require('http');
+var server = http.createServer(app);
+var mongoose = require('mongoose');
+
+//Conexión a la base de datos
+mongoose.connect('mongodb+srv://diego:diego123@cluster0-hflvi.gcp.mongodb.net/test?retryWrites=true&w=majority', function (err, res) {
+  if (err) throw err;
+  console.log('Conectándose a mi base!');
+});
+
+//Middelwares (investigar que es)
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+var cors = require('cors');
+app.use(cors());
+
+routes = require('./routes/librerias.js')(app);
+
+app.get('/', function (req, res) {
+  res.send("Welcome to the machine");
+});
+
+server.listen(process.env.PORT || 3000, function () {
+  console.log("Servidor corriendo en localhost:3000");
+});
+
+module.export = app;
